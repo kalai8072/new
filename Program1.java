035e5c57623c151a62c72ca3c070b95a40154c1f
Kalai <vijaykalaiselvan1997@gmail.com>
2023-11-18 20:44:02 +0530

    pattern

---
+++ Program1.java
@@ -0,0 +1,32 @@
+package Patternprograms;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=10;i++) {
+			for (int j=1;j<=i;j++) {
+				
+				System.out.print("*");
+				
+		
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
+
+    /*  for (int i=1; i<=5; i++) {
+    	  for (int j=5; j>=i; j--) {
+    		  
+    		  System.out.print("*");
+    		  
+    	  }
+    	  System.out.println();
+      }
+	}
+}*/
